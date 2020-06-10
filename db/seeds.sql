0ae770f24cd4fde923599b5b217d738696e94ffd
Drake24 <zjhassler@gmail.com>
2020-06-09 19:24:43 -0500

    setup db

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Dominator', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeze Whiz', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('You Are Unworthy', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cowamegeddon', FALSE);
