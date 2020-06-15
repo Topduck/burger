c373edb20b479c461124123a7147b2f110204db4
Drake24 <zjhassler@gmail.com>
2020-06-14 19:49:52 -0500

    added controllers.  Working on setting up handlebars.

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -18,7 +18,36 @@ router.get("/", function(req, res) {
 });
  // post a new burger
 router.post("/api/burgers", function(res, req) {
-    burger.create(
-        
-    )
-}
+    burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result){
+        res.json({id: result.insertId});
+    });
+});
+// reseting a burger as devoured
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.updateOne({ devoured: req.body.devoured }, condition, function(result) {
+        if (result.changedRows === 0) {
+            // If no burgers are changed, then the ID must not exist, so 404.
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+//delete a burger from the db
+router.delete("/api/burgers/:id", function(req, res){
+    burger.deleteOne(condition, function(result){
+        if(result.changedRows === 0) {
+            //if no burgers were deleted this will catch the 404 error.
+            return res.status(404).end();
+        }
+        else{
+            res.status(200).end();
+        }
+    })
+});
+
+modeule.exports = router
